32f9149604ff52dfb8488bf6adb497f352e4d40b
Shubham Singh <shub.singh007@gmail.com>
2021-03-13 15:43:34 +0530

    code reformating

--- src/main/java/com/coderscafe/sturdyeureka/core/unionfind/AccountsMerge.java
+++ src/main/java/com/coderscafe/sturdyeureka/core/unionfind/AccountsMerge.java
@@ -4,25 +4,25 @@ import java.util.*;
 
 public class AccountsMerge {
 
-    public List<List<String>> mergeAccounts(List<List<String>> accounts){
+    public List<List<String>> mergeAccounts(List<List<String>> accounts) {
         DSU dsu = new DSU();
-        Map<String,String> emailToName = new HashMap<>();
-        for(List<String> account : accounts){
+        Map<String, String> emailToName = new HashMap<>();
+        for (List<String> account : accounts) {
             String name = "";
-            for(String email : account){
-                if(name.isEmpty()){
+            for (String email : account) {
+                if (name.isEmpty()) {
                     name = email;
                     continue;
                 }
-                emailToName.put(email,name);
-                dsu.union(account.get(1),email);
+                emailToName.put(email, name);
+                dsu.union(account.get(1), email);
             }
         }
         Map<String, List<String>> ans = new HashMap<>();
-        for (String email: emailToName.keySet()) {
-            ans.computeIfAbsent(dsu.find(email), x-> new ArrayList<>()).add(email);
+        for (String email : emailToName.keySet()) {
+            ans.computeIfAbsent(dsu.find(email), x -> new ArrayList<>()).add(email);
         }
-        for (List<String> component: ans.values()) {
+        for (List<String> component : ans.values()) {
             Collections.sort(component);
             component.add(0, emailToName.get(component.get(0)));
         }
--- src/main/java/com/coderscafe/sturdyeureka/core/unionfind/DSU.java
+++ src/main/java/com/coderscafe/sturdyeureka/core/unionfind/DSU.java
@@ -5,20 +5,21 @@ import java.util.Map;
 
 public class DSU {
 
-     private final Map<String,String> parentChildMap ;
+    private final Map<String, String> parentChildMap;
 
-     public DSU(){
-         parentChildMap = new HashMap<>();
-     }
+    public DSU() {
+        parentChildMap = new HashMap<>();
+    }
 
-     public String find(String email){
-         parentChildMap.putIfAbsent(email, email);
-         if(!parentChildMap.get(email).equals(email)){
-             return find(parentChildMap.get(email));
-         }
-         return email;
-     }
-     public void union(String emailOne, String emailTwo){
-         parentChildMap.put(find(emailOne),find(emailTwo));
-     }
+    public String find(String email) {
+        parentChildMap.putIfAbsent(email, email);
+        if (!parentChildMap.get(email).equals(email)) {
+            return find(parentChildMap.get(email));
+        }
+        return email;
+    }
+
+    public void union(String emailOne, String emailTwo) {
+        parentChildMap.put(find(emailOne), find(emailTwo));
+    }
 }
